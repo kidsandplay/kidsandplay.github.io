1a9d8a277bdbf42160395fadf0ca4dba6f539b3a
kidsandplay <115195138+kidsandplay@users.noreply.github.com>
2022-10-07 01:06:35 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,21 +32,7 @@
 			speed: 300
 		});
 
-		// Scrolly.
-		$('.scrolly').scrolly({
-			speed: 1000,
-			offset: function() {
-
-				// If <=large, >small, and sidebar is present, use its height as the offset.
-					if (breakpoints.active('<=large')
-					&&	!breakpoints.active('<=small')
-					&&	$sidebar.length > 0)
-						return $sidebar.height();
-
-				return 0;
-
-			}
-		});
+			
 		
 
 	// Nav.
